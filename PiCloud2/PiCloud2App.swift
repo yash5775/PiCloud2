1f529726f39f093faf02d7b194b03bbae6efb38a
yashkumar <yashchaniyara123@gmail.com>
2025-05-12 17:26:47 +0530

    Initial Commit

---
+++ PiCloud2/PiCloud2App.swift
@@ -0,0 +1,17 @@
+//
+//  PiCloud2App.swift
+//  PiCloud2
+//
+//  Created by Chaniyara Yash on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PiCloud2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiCloud2Tests/PiCloud2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PiCloud2Tests.swift
+//  PiCloud2Tests
+//
+//  Created by Chaniyara Yash on 12/05/25.
+//
+
+import Testing
+@testable import PiCloud2
+
+struct PiCloud2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
